51618d85df1218e3f791f77f3754a45194581989
Agnese Portera <agneseportera@gmail.com>
2021-02-27 22:13:19 +0100

    Add message resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 15 },
               uniqueness: { case_sensitive: false }
   has_secure_password
+  has_many :messages
 end
